9a3810f60e46fa6ab9dc7329a35dba3702249275
Maheswari Maddi <maddimaheswari111@gmail.com>
2024-04-03 14:51:06 +0530

    functions in js

---
+++ functions.js
@@ -0,0 +1,12 @@
+
+function emp(){
+    if(true){
+        let y=10
+        console.log("let varible scope is with in the block",y)
+            }
+        // console.log("let varible out of block will not accesble it throws reference error",y)    
+}
+emp()
+
+
+
