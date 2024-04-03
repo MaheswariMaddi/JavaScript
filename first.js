f8564645dc2f7d84f59c8ecc1b9a8ed5e0b7af69
Maheswari Maddi <maddimaheswari111@gmail.com>
2024-04-03 14:50:45 +0530

    First  prog

---
+++ first.js
@@ -0,0 +1,18 @@
+console.log("Hello Javascript")
+// Type Coercion
+console.log(5+true)
+console.log("hello"+3)
+console.log([]+3)
+console.log(null+1)
+console.log(null+'1')
+console.log(undefined+1)
+console.log(1==true)
+// const a=10 
+a=11
+console.log(a)
+console.log("Before Initilazition",v)
+var v=100
+console.log("After Declaration",v)
+
+
+
